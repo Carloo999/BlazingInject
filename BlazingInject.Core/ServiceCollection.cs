541a89b82c8778edc669c46f7ee025ac3bed84ca
Carlo <carlo.bene@web.de>
2023-10-24 22:09:13 +0200

    Added support for service implementation instances and factory methods

--- BlazingInject.Core/ServiceCollection.cs
+++ BlazingInject.Core/ServiceCollection.cs
@@ -4,16 +4,85 @@ namespace BlazingInject.Core;
 public class ServiceCollection : List<ServiceDescriptor>
 {
 
+    public ServiceCollection AddService(ServiceDescriptor descriptor)
+    {
+        Add(descriptor);
+        return this;
+    }
+
+    public ServiceCollection AddSingleton<TService>(Func<ServiceProvider, TService> factory)
+        where TService : class
+    {
+        var serviceDescriptor = new ServiceDescriptor
+        {
+            ServiceType = typeof(TService),
+            ImplementationType = typeof(TService),
+            ImplementationFactory = factory,
+            Lifetime = ServiceLifetime.Singleton
+        };
+        Add(serviceDescriptor);
+        return this;
+    }
+
+    public ServiceCollection AddSingleton(object implementation)
+    {
+        var serviceType = implementation.GetType();
+        
+        var serviceDescriptor = new ServiceDescriptor
+        {
+            ServiceType = serviceType,
+            ImplementationType = serviceType,
+            Implementation = implementation,
+            Lifetime = ServiceLifetime.Singleton
+        };
+        Add(serviceDescriptor);
+        return this;
+    }
+    
+    public ServiceCollection AddSingleton<TService>()
+        where TService : class
+    {
+        ServiceDescriptor serviceDescriptor = AddServiceDescriptorWithLifetime<TService, TService>(ServiceLifetime.Singleton);
+        Add(serviceDescriptor);
+        return this;
+    }
+    
     public ServiceCollection AddSingleton<TService, TImplementation>()
+        where TService : class
+        where TImplementation : class, TService
     {
         ServiceDescriptor serviceDescriptor = AddServiceDescriptorWithLifetime<TService, TImplementation>(ServiceLifetime.Singleton);
         Add(serviceDescriptor);
         return this;
     }
     
+    public ServiceCollection AddTransient<TService>(Func<ServiceProvider, TService> factory)
+        where TService : class
+    {
+        var serviceDescriptor = new ServiceDescriptor
+        {
+            ServiceType = typeof(TService),
+            ImplementationType = typeof(TService),
+            ImplementationFactory = factory,
+            Lifetime = ServiceLifetime.Transient
+        };
+        Add(serviceDescriptor);
+        return this;
+    }
+    
     public ServiceCollection AddTransient<TService, TImplementation>()
+        where TService : class
+        where TImplementation : class, TService
     {
         ServiceDescriptor serviceDescriptor = AddServiceDescriptorWithLifetime<TService, TImplementation>(ServiceLifetime.Transient);
+        Add(serviceDescriptor);
+        return this;
+    }
+    
+    public ServiceCollection AddTransient<TService>()
+        where TService : class
+    {
+        ServiceDescriptor serviceDescriptor = AddServiceDescriptorWithLifetime<TService, TService>(ServiceLifetime.Transient);
         Add(serviceDescriptor);
         return this;
     }
--- BlazingInject.Core/ServiceDescriptor.cs
+++ BlazingInject.Core/ServiceDescriptor.cs
@@ -5,5 +5,7 @@ public class ServiceDescriptor
     public ServiceLifetime Lifetime { get; init; }
     public Type ServiceType { get; init; } = default!;
     public Type ImplementationType { get; set; }
+    public Func<ServiceProvider, object>? ImplementationFactory { get; set; }
+    public object? Implementation { get; set; }
 
 }
