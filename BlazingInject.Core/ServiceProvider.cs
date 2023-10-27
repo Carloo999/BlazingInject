5010d450cb6569eb78792b9cd5ee08f730e436ac
Carlo <carlo.bene@web.de>
2023-10-27 19:41:29 +0200

    Update ServiceProvider for multiple instance support
    The changes in ServiceProvider now allow for multiple instances of singleton and transient services by storing them in lists. Added GetServices methods to retrieve all single or multiple instances of a specific service. Updated the generation of services to append new instances to the list of either singleton or transient services instead of replacing the previous value.

--- BlazingInject.Core/ServiceProvider.cs
+++ BlazingInject.Core/ServiceProvider.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Collections;
+using System.Reflection;
 
 namespace BlazingInject.Core;
 
 public class ServiceProvider
 {
-    private readonly Dictionary<Type, Func<object>> _transientTypes = new();
-    private readonly Dictionary<Type, Lazy<object>> _singletonTypes = new();
+    private readonly Dictionary<Type, List<Func<object>>> _transientTypes = new();
+    private readonly Dictionary<Type, List<Lazy<object>>> _singletonTypes = new();
     internal ServiceProvider(ServiceCollection serviceCollection)
     {
         GenerateServices(serviceCollection);
@@ -16,16 +17,51 @@ public class ServiceProvider
         return (T?)GetService(typeof(T));
     }
 
+    public T?[] GetServices<T>()
+    {  
+        var services = GetServices(typeof(T));
+        var servicesCasted = new List<T?>();
+
+        foreach (var service in services)
+        {
+            servicesCasted.Add((T?)service);
+        }
+
+        return servicesCasted.ToArray();
+    }
+
+    public object[]? GetServices(Type serviceType)
+    {
+        var singletonService = _singletonTypes.GetValueOrDefault(serviceType)?.Select(x => x.Value);
+        var services = new List<object>();
+        
+        if (singletonService is not null)
+        {
+            services = services.Concat(singletonService).ToList();
+        }
+
+        var transientService = _transientTypes.GetValueOrDefault(serviceType);
+
+        if (transientService is null) return services.ToArray();
+        
+        foreach (Func<object> transient in transientService)
+        {
+            services.Add(transient.Invoke());
+        }
+
+        return services.ToArray();
+    }
+
     public object? GetService(Type serviceType)
     {
-        var singletonService = _singletonTypes.GetValueOrDefault(serviceType);
+        Lazy<object>? singletonService = _singletonTypes.GetValueOrDefault(serviceType)?.Last();
 
         if (singletonService is not null)
         {
             return singletonService.Value;
         }
 
-        var transientService = _transientTypes.GetValueOrDefault(serviceType);
+        var transientService = _transientTypes.GetValueOrDefault(serviceType)?.Last(); 
         return transientService?.Invoke();
     }
 
@@ -36,38 +72,49 @@ public class ServiceProvider
             switch (serviceDescriptor.Lifetime)
             {
                 case ServiceLifetime.Singleton:
+
+                    if (_singletonTypes.GetValueOrDefault(serviceDescriptor.ServiceType) is null)
+                    {
+                        _singletonTypes[serviceDescriptor.ServiceType] = new List<Lazy<object>>();
+                    }
+                    
                     if (serviceDescriptor.Implementation is not null)
                     {
-                        _singletonTypes[serviceDescriptor.ServiceType] = 
-                            new Lazy<object>(serviceDescriptor.Implementation);
+                        _singletonTypes[serviceDescriptor.ServiceType].
+                            Add(new Lazy<object>(serviceDescriptor.Implementation));   
                         continue;
                     }
 
                     if (serviceDescriptor.ImplementationFactory is not null)
                     {
-                        _singletonTypes[serviceDescriptor.ServiceType] =
-                            new Lazy<object>(() =>
-                                serviceDescriptor.ImplementationFactory(this));
+                        _singletonTypes[serviceDescriptor.ServiceType].
+                            Add(new Lazy<object>(() =>
+                            serviceDescriptor.ImplementationFactory(this)));
                         continue;
                     }
                     
-                    _singletonTypes[serviceDescriptor.ServiceType] =
-                        new Lazy<object>(() => 
+                    _singletonTypes[serviceDescriptor.ServiceType].
+                        Add(new Lazy<object>(() => 
                             Activator.CreateInstance(serviceDescriptor.ImplementationType,
-                            GetConstructorParameters(serviceDescriptor))!);
+                                GetConstructorParameters(serviceDescriptor))!));
                     continue;
                 case ServiceLifetime.Transient:
 
+                    if (_transientTypes.GetValueOrDefault(serviceDescriptor.ServiceType) is null)
+                    {
+                        _transientTypes[serviceDescriptor.ServiceType] = new List<Func<object>>();
+                    }
+                    
                     if (serviceDescriptor.ImplementationFactory is not null)
                     {
-                        _transientTypes[serviceDescriptor.ServiceType] =
-                            () => serviceDescriptor.ImplementationFactory(this);
+                        _transientTypes[serviceDescriptor.ServiceType].
+                            Add(() => serviceDescriptor.ImplementationFactory(this));
                         continue;
                     }
 
-                    _transientTypes[serviceDescriptor.ServiceType] =
-                        () => Activator.CreateInstance(serviceDescriptor.ImplementationType,
-                                GetConstructorParameters(serviceDescriptor))!;
+                    _transientTypes[serviceDescriptor.ServiceType].
+                        Add(() => Activator.CreateInstance(serviceDescriptor.ImplementationType,
+                            GetConstructorParameters(serviceDescriptor))!);
                     continue;
             }
         }
